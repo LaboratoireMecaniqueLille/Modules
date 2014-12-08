024f52d2398b22a686b9e925e0f12cb1ab074a9c
CMartelLML <corentin.martel@univ-lille1.fr>
2014-12-08 09:17:44 +0100

    Modifications:   - IR module can now process images in DL without switching to Flux   - help as been added for each function

--- lml/InfraRed.py
+++ lml/InfraRed.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 import scipy
-import pyradi
 from scipy.optimize import leastsq
 import matplotlib.pyplot as plt
 import math as m
@@ -13,7 +12,7 @@ io.use_plugin('freeimage')
 
 
 class Calibration:
-  def __init__(self,sigma,BB_Emissivity,directory,result_directory,video_directory,Tmin,Tmax,Tstep,polynomial_degree,bad_pix_critere,area_size,NUC,extension,camera): 
+  def __init__(self,sigma,BB_Emissivity,directory,result_directory,video_directory,Tmin,Tmax,Tstep,polynomial_degree,bad_pix_critere,area_size,NUC,Flux,extension,camera): 
     self.sigma = sigma # sigma is use in the boltzman equation
     self.BB_Emissivity = BB_Emissivity
     self.directory= directory # directory of th BB images for calibration
@@ -26,26 +25,29 @@ class Calibration:
     self.bad_pix_critere=bad_pix_critere
     self.area_size=area_size# size, in pixels, of the side of the square area used for the mean(DL) in the NUC 
     self.NUC=NUC # set True if you want to use the NUC (DL=>DL) function
+    self.Flux=Flux # set True if you want to convert DL to flux
     self.extension=extension
     self.camera=camera
 
 
 # # # # Definition of the functions
 
-# This function imports ths BB-image and shape them in vectors. 
-  #Returns: 
-      #imageBB[i]: the original array of DL, where i is the T°C
   def import_imageBB(self):
+"""
+  This function imports ths BB-image and shape them in vectors. 
+  Returns:  
+    imageBB[i]: the original array of DL, where i is image number
+"""
     imageBB={}
     imageBB_={}
     if self.extension=="ptm":
-      for i in range(0,(self.Tmax-self.Tmin+self.Tstep),self.Tstep):
-	imageBB_[i]=PTW.GetPTW(self.directory+str(i+self.Tmin)+".ptm")
+      for i in range(0,(self.Tmax-self.Tmin)/self.Tstep):
+	imageBB_[i]=PTW.GetPTW(self.directory+str(i*self.Tstep+self.Tmin)+".ptm")
 	imageBB_[i].get_frame(0)
 	imageBB[i]=1.*imageBB_[i].frame_data 
     if self.extension=="ptw":
-      for i in range(0,(self.Tmax-self.Tmin+self.Tstep),self.Tstep):
-	imageBB_[i]=PTW.GetPTW(self.directory+str(i+self.Tmin)+".ptw")
+      for i in range(0,(self.Tmax-self.Tmin)/self.Tstep):
+	imageBB_[i]=PTW.GetPTW(self.directory+str(i*self.Tstep+self.Tmin)+".ptw")
 	nbr_of_frames=imageBB_[i].number_of_frames
 	imageBB[i]=0
 	for j in range(nbr_of_frames):  # This loop calculate the mean of the images. use it if you didn't save in .ptm
@@ -53,12 +55,15 @@ class Calibration:
 	  imageBB[i]+=(1.*imageBB_[i].frame_data)/(1.*nbr_of_frames)
     return imageBB
 
-# This shape images into vectors. 
-  #Returns: 
-      # matrice: an array in wich each line is a vectorised BB-image, one line by T°C
-      #DL_mean[i]: the mean DL in the central area, for the NUC function. i depend on the T°C
-      #shape : the original shape of ths images
+
   def reshape(self,DL):
+"""
+ This shape images into vectors. 
+  Returns: 
+      matrice: an array in wich each line is a vectorised BB-image, one line by T°C
+      DL_mean[i]: the mean DL in the central area, for the NUC function. i depend on the T°C
+      shape : the original shape of ths images
+"""
     imageBB=DL
     imageBB2={}
     matrice=[]
@@ -94,12 +99,14 @@ class Calibration:
     return matrice, DL_mean, shape
 
 
-#This function calculate the NUC coefficient for each pixels, based on the central-zone mean DL
-  #Returns:
-      #The matrix of vectorised BB-images, with the NUC applied
-  #Save:
-      #Save the NUC coefficients in the result directory, under coeffs_NUC.pyc
   def NUC_DL(self,DL,DL_mean):
+"""
+This function calculate the NUC coefficient for each pixels, based on the central-zone mean DL
+  Returns:
+      The matrix of vectorised BB-images, with the NUC applied
+  Save:
+      Save the NUC coefficients in the result directory, under coeffs_NUC.pyc
+"""
     DLC_NUC=copy.copy(DL)
     a,d = np.shape(DL)
     interp_degree=3
@@ -113,12 +120,14 @@ class Calibration:
     return DLC_NUC
 
 
-#This function calculate the calibration coefficients for each pixels
-  #Returns:
-      #The matrix of vectorised BB-images, with the (NUC and) calibration applied
-  #Save:
-      #Save the calibration coefficients in the result directory, under coeffs_DL2Flux.pyc
   def DL2Flux(self,DL):
+"""
+This function calculate the calibration coefficients for each pixels
+  Returns:
+      The matrix of vectorised BB-images, with the (NUC and) calibration applied
+  Save:
+      Save the calibration coefficients in the result directory, under coeffs_DL2Flux.pyc
+"""
     DLC_Calibrated=copy.copy(DL)
     a,d=np.shape(DL)
     T = np.arange(self.Tmin,(self.Tmax+self.Tstep),self.Tstep)
@@ -133,12 +142,15 @@ class Calibration:
     return DLC_Calibrated
 
 
-#This function spots the bad pixels in each image, and set their value to 0. It keeps the bad pixels of the worst image(with the maximum numbers of BP)
-  #Returns:
-    #The matrix of vectorised BB-images, with all transformations (NUC,calibration and BP) applied
-    #mouchard_final : the matrix of bad pixels (1 =good, 0=bad)
-    #last_nbr_BP : the number of bad pixels spoted
   def bad_pixels(self,DL,shape):
+"""
+This function spots the bad pixels in each image, and set their value to 0. It keeps the bad pixels of the worst image(with the maximum numbers of BP)
+  Returns:
+    DL_final, wich is the DL with mean value replacing the bad pixels. You are not supposed to use this for anything other than display purpose.
+    mouchard_final : the matrix of bad pixels (1 =good, 0=bad)
+    last_nbr_BP : the number of bad pixels spoted
+  Saves the mouchard matrix in the result directory.
+"""
     DL_final={}
     DL_mat={}
     DL_reshaped=[]
@@ -146,28 +158,29 @@ class Calibration:
     best_mouchard=[]
     
     
-    for i in range(0,(self.Tmax-self.Tmin+self.Tstep),self.Tstep):
+    for i in range(0,(self.Tmax-self.Tmin)/self.Tstep):
       DL_mat[i]=np.reshape(DL[i],shape)
       DL_mat[i],mouchard,nbr_BP=self.bad_pixels_detection_grad(DL_mat[i])
       DL_reshaped.append(np.reshape(DL_mat[i],(1,shape[0]*shape[1])))
       if nbr_BP>last_nbr_BP:
 	best_mouchard=mouchard
 	last_nbr_BP=nbr_BP
-    DL_final = np.concatenate([DL_reshaped[i] for i in range(0,(self.Tmax-self.Tmin+self.Tstep),self.Tstep)],axis=0)
+    DL_final = np.concatenate([DL_reshaped[i] for i in range(0,(self.Tmax-self.Tmin)/self.Tstep)],axis=0)
     mouchard_final=np.reshape(best_mouchard,(1,shape[0]*shape[1]))
     np.save(self.result_directory+'mouchard',mouchard_final)
     return DL_final,mouchard_final,last_nbr_BP
 
 
-#This function is called by the bad_pixels function. It spots the bad pixels, based on the gradient of energy of each pixel
+
   def bad_pixels_detection_grad(self,DL):
+"""
+This function is called by the bad_pixels function. It spots the bad pixels, based on the gradient of energy of each pixel
+"""
     DL_corrected=copy.copy(DL)
     DLC_NUC=copy.copy(DL)
     b,c = np.shape(DL)
     mouchard=np.zeros((b,c));
     nbr_BP=0;
-
-    #%calcul du gradient
 
     gx,gy=np.gradient(DL)
     E=(gx*gx+gy*gy)**0.5
@@ -188,27 +201,39 @@ class Calibration:
     return DLC_NUC,mouchard,nbr_BP
 
 
-#This function applies all the transformations (NUC, Calibration, and BP) to an image(NOT vectorised), with the coefficients stored in the result directory     
-  # return:
-      #T_map-mreshaped: map of temperatures 
+
   def apply_coeffs(self,DL):
+"""
+This function applies all the transformations (NUC, Calibration, and BP) to an image(NOT vectorised), with the coefficients stored in the result directory     
+   return:
+      masked_reshaped: map of temperatures if Flux=True, otherwise it's a map of the DL with NUC applied
+"""
     b,c=np.shape(DL[0])
     DL_vect, DL_mean, shape=self.reshape(DL)
-    DL2Flux=np.transpose(np.load(self.result_directory+'coeffs_DL2Flux.npy'))
     mouchard=np.load(self.result_directory+'mouchard.npy')
     if self.NUC==True:
       self.NUC_coeffs=np.transpose(np.load(self.result_directory+'coeffs_NUC.npy'))
       DLC_NUC=np.polyval(self.NUC_coeffs,DL_vect)
-      flux=np.polyval(DL2Flux,DLC_NUC)
+      matrix=DLC_NUC
+      if self.Flux==True:
+	DL2Flux=np.transpose(np.load(self.result_directory+'coeffs_DL2Flux.npy'))
+	flux=np.polyval(DL2Flux,DLC_NUC)
     else:
-      flux=np.polyval(DL2Flux,DL_vect)
-    T=((flux/(self.BB_Emissivity*self.sigma))**(0.25)-273.16)  
-    T_map=ma.masked_array(T,mouchard,fill_value=0)  ### applique une matrice mask
-    T_map_reshaped=np.reshape(T_map,(b,c))  
-    return T_map_reshaped
+      if self.Flux==True:
+	DL2Flux=np.transpose(np.load(self.result_directory+'coeffs_DL2Flux.npy'))
+	flux=np.polyval(DL2Flux,DL_vect)
+    if self.Flux== True:
+      T=((flux/(self.BB_Emissivity*self.sigma))**(0.25)-273.16)  
+      matrix=T
+    masked=ma.masked_array(matrix,mouchard,fill_value=0)  ### applique une matrice mask
+    masked_reshaped=np.reshape(masked,(b,c))  
+    return masked_reshaped
     
-#This function plots the temperature-map, with clim=[Low-limit,High-limit] of the colorbar
+
   def heat_map(self,DL):
+"""
+This function plots the image with colorbar and 3-standart-deviation rule.
+"""
     fig = plt.figure()
     ax = fig.add_subplot(1,1,1)
     plt.imshow(DL)
@@ -216,46 +241,68 @@ class Calibration:
     plt.colorbar()
     plt.show(block=False)
     
-#This function displays the BB-images after applying all the calibration-corrections.
-  def verif_calibration(self,imageBB):  # This loop evaluate and show every BB-image imported 
+
+  def verif_calibration(self,imageBB):
+"""
+This function displays the BB-images after applying all the calibration-corrections.
+"""
     T_map_reshaped={}
     DL={}
-    for i in range(0,(self.Tmax-self.Tmin+self.Tstep),self.Tstep): 
+    for i in range(0,(self.Tmax-self.Tmin)/self.Tstep): 
       DL[0]=imageBB[i]
       T_map_reshaped[i]=self.apply_coeffs(DL)
       self.heat_map(T_map_reshaped[i])
       
-#This function applies the calibration to ALL the images in the essay. MAKE SURE your calibration is right before launching this. 
-#Images are saved in .tiff 16 bits, wich allow to store temperature information directly inside with mK precision. Temperature = pixelValue /1000.
+
   def apply_to_essay(self,save_tif,video):
-    test1=PTW.GetPTW(video)
+"""
+This function applies the calibration to ALL the images in the essay. MAKE SURE your calibration is right before launching this. 
+Images are saved in .tiff 16 bits, wich allow to store temperature information directly inside:
+    - for T<65°C. Temperature = pixelValue /1000. mK precision
+    - for 65<T<650 , Temperature = Value/100, 10mK precision
+    - for T >650, Temperature = Value /10, 100mK precision
+"""
+    test1=PTW.GetPTW(self.video_directory+video)
     frame_nbr=test1.number_of_frames
     frame_rate=test1.frame_rate
     frame={}
     for i in range (0,frame_nbr):
       test1.get_frame(i)
       frame[0]=(test1.frame_data)
+      print frame[0].max()
       T_map2=self.apply_coeffs(frame)
-      T_map=np.clip(T_map2,self.Tmin,self.Tmax)
-      T_map3=np.around(T_map,decimals=3)
-      T_map4=(T_map3*1000).astype(np.uint16)
+      if self.Flux==True:
+	T_map=np.clip(T_map2,self.Tmin,self.Tmax)
+	power=np.floor(np.log10(2**16/(max(T_map)))) # For storing Temperature information in a 16 bits tiff as integer 
+	T_map2=(np.around(T_map,decimals=power))*10**power
+      T_map3=(T_map2).astype(np.uint16)
+      print T_map2.max()
+      print T_map3.max()
       if save_tif==True:
-	io.imsave(self.result_directory+"IR_images/"+"img_"+str(i)+".tiff",T_map4) #.astype(np.int16)
+	io.imsave(self.result_directory+"IR_images/"+"img_"+str(video)+".tiff",T_map3) #.astype(np.int16)
 
 
-  def apply_to_essay_mean(self,save_tif,video,video_nbr):
-    test1=PTW.GetPTW(video)
+  def apply_to_essay_mean(self,save_tif,video):
+"""
+This function applies the calibration to ALL the images in the essay and evaluate the mean value for each pixels. MAKE SURE your calibration is right before launching this. 
+Images are saved in .tiff 16 bits, wich allow to store temperature information directly inside:
+    - for T<65°C. Temperature = pixelValue /1000. mK precision
+    - for 65<T<650 , Temperature = Value/100, 10mK precision
+    - for T >650, Temperature = Value /10, 100mK precision
+"""
+    test1=PTW.GetPTW(self.video_directory+video)
     frame_nbr=test1.number_of_frames
     frame[0]=0
     for i in range (0,frame_nbr):
       test1.get_frame(i)
       frame[0]+=(test1.frame_data)/(1.*frame_nbr)
     T_map2=self.apply_coeffs(frame)
-    T_map=np.clip(T_map2,self.Tmin,self.Tmax)
-    T_map2=np.around(T_map,decimals=3)
-    T_map3=(T_map2*1000).astype(np.uint16)
+    if self.Flux==True:
+      T_map=np.clip(T_map2,self.Tmin,self.Tmax)
+      power=np.floor(np.log10(2**16/(max(T_map)))) # For storing Temperature information in a 16 bits tiff as integer 
+      T_map2=(np.around(T_map,decimals=power))*10**power
+    T_map3=(T_map2).astype(np.uint16)
     if save_tif==True:
-	io.imsave(self.result_directory+"IR_images/"+"Dep_"+str(video_nbr)+"_mean.tiff",T_map3) #.astype(np.int16)
-	#plt.imsave(self.result_directory+"IR_images/"+"Dep_"+str(j)+"_mean_plt.tiff",T_map3,cmap=plt.cm.gray)
+	io.imsave(self.result_directory+"IR_images/"+"img_"+str(video)+"_mean.tiff",T_map3) #.astype(np.int16)
 
 
